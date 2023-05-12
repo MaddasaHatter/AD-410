bdc2007d51ad63a3359cacbda980860e5378ec63
Lilith Lucifer <lilith.caldwell@seattlecolleges.edu>
2023-05-11 21:11:33 -0700

    many to many vid 4

---
+++ PokemonReviewApp/PokemonReviewApp/Models/PokemonCatergory.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace PokemonReviewApp.Models
+{
+    public class PokemonCatergory
+    {
+        public  int Id { get; set; }
+        public  int CategoryId { get; set; }
+        public  Pokemon Pokemon { get; set; }
+        public  Catergory Catergory  { get; set; }
+    }
+}
