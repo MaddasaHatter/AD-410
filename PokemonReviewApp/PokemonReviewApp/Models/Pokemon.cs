54e491d077c89f84968cc9953125992081eb08f0
Lilith Lucifer <lilith.caldwell@seattlecolleges.edu>
2023-05-11 20:18:42 -0700

    database relationships vid 3

--- PokemonReviewApp/PokemonReviewApp/Models/Pokemon.cs
+++ PokemonReviewApp/PokemonReviewApp/Models/Pokemon.cs
@@ -5,6 +5,8 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime BirthDate { get; set; }
+        public  ICollection<Review> Reviews { get; set; }
+
 
     }
 }
